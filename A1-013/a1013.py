5c1a1edb91d7c4f0fc717445184bd4e8ce9f2bb5
boatengine <thanapat.boat16@gmail.com>
2025-05-25 22:07:30 +0700

    a1013

---
+++ A1-013/a1013.py
@@ -0,0 +1,11 @@
+x,y = input(),input()
+
+
+if x == "H" and y == "4567":
+    print("safe unlocked")
+elif x == "H":
+    print("safe locked - change digit")
+elif y == "4567":
+    print("safe locked - change char")
+else:
+    print("safe locked")
